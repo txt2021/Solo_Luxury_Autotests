a60952874693286e09b10c7f2a1cc3c9ccad3d8c
Tetiana Chuiasova <txt2021@gmail.com>
2022-12-15 14:46:30 +0200

    add tests for footer functionalities

--- test/pageobjects/main.page.js
+++ test/pageobjects/main.page.js
@@ -10,7 +10,43 @@ const invalid_search_text = 'Взуття';
 const search_button = ('[id="search_mini_form"]');
 const special_request_button = ('[id="myspecialreq"]');
 
+const newsletter_input = ('[id="newsletter"]');
+const newsletter_input_text = require('random-email');;
+const subscribe_button = ('[class="sub-bttn"]');
+const success_message_field = ('[id="newsletter"]~span');
+const success_message_text = "You've been added to our mailing list.";
+const invalid_newsletter_text = 'InapropEmail';
+const already_taken_email = 'tara@ceoshub.com';
+const error_message_text = 'This email address is already subscribed';
+
+const about_us_button = ('[class="col-lg-2"]:nth-child(1)>div strong');
+const app_button = ('[class="col-lg-2"]:nth-child(1)>div ul>li:nth-child(5)>a');
+const contact_button = ('[class="col-lg-2"]:nth-child(2)>div strong');
+const shipping_button = ('[class="col-lg-2"]:nth-child(2)>div ul>li:nth-child(3)>a');
+const social_button = ('[class="col-lg-2"]:nth-child(3)>div strong');
+const hope_button = ('[class="col-lg-2"]:nth-child(3)>div ul>li:nth-child(1)>a');
+const company_button = ('[class="col-lg-2"]:nth-child(4)>div strong');
+const testimonials_button = ('[class="col-lg-2"]:nth-child(4)>div ul>li:nth-child(4)>a');
+const error_message = ('[id="authenticationPopup"]~div h3');
+
+
 class MainPage {
+
+    async hoverToAboutUs(){
+        await page.hover(about_us_button);
+    }
+
+    async hoverToContact(){
+        await page.hover(contact_button);
+    }
+
+    async hoverToCompany(){
+        await page.hover(company_button);
+    }
+
+    async hoverToSocial(){
+        await page.hover(social_button);
+    }
 
     async clickpersonalAccountButton(){
         await page.click(personal_accountButton);        
@@ -32,6 +68,26 @@ class MainPage {
         await page.click(special_request_button);        
     }
 
+    async clickSubscribeButton(){
+        await page.click(subscribe_button);        
+    }
+
+    async clickAppButton(){
+        await page.click(app_button);        
+    }
+
+    async clickShippingButton(){
+        await page.click(shipping_button);        
+    }
+
+    async clickHopeButton(){
+        await page.click(hope_button);        
+    }
+
+    async clickTestimonialsButton(){
+        await page.click(testimonials_button);        
+    }
+
     async enterSearchKeyword(){
             await page.setvalue(search_field,search_text);     
         }
@@ -39,6 +95,56 @@ class MainPage {
     async enterInvalidSearchKeyword(){
             await page.setvalue(search_field,invalid_search_text);     
         }
+    
+     async enterNewsletterEmail(){
+        await page.setvalue(newsletter_input,newsletter_input_text());     
+    }
+
+    async enterTakenNewsletterEmail(){
+        await page.setvalue(newsletter_input,already_taken_email);     
+    }
+
+    async enterInvalidNewsletterEmail(){
+        await page.setvalue(newsletter_input,invalid_newsletter_text);     
+    }
+
+    async isNewsletterSuccessMessageVisible(){
+        await (expect(page.isdisplayed(success_message_field)));
+    }
+
+    async isNewsletterSuccessMessageCorrect(){
+        await (expect(page.checkElement(success_message_field,success_message_text)));
+    }
+
+    async isNewsletterErrorMessageVisible(){
+        await (expect(page.isdisplayed(success_message_field)));
+    }
+
+    async isNewsletterErrorMessageCorrect(){
+        await (expect(page.checkElement(success_message_field,error_message_text)));
+    }
+
+    async isSubscribeButtonClickable(){
+        await (expect((page.isclickable(subscribe_button))==false));
+    }
+
+
+    async urlAppChecking(){
+        await (expect(page.urlChecking('app')));
+
+    }
+
+    async urlShippingChecking(){
+        await (expect(page.urlChecking('shipping')));
+    }
+
+    async urlHopeChecking(){
+        await (expect(page.urlChecking('hope')));
+    }
+
+    async urlTestimonialsChecking(){
+        await (expect(page.urlChecking('testimonials'))) || (expect(page.isdisplayed(error_message)));
+    }
 
 }
 
